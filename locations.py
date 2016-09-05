a227a2469f5910a11e55576b289d7f8ece1c9f19
Christian <cschomak@cern.ch>
2016-09-05 11:22:17 +0200

    locations file added

---
+++ locations.py
@@ -0,0 +1,6 @@
+class locations:
+	
+	masterListPath = "/afs/desy.de/user/s/school65/CMSDAS/SUSY-Edge/frameWorkBase"
+	dataSetPath = "/nfs/dust2/cms/group/DAS2016/SUSYEdge/CMSDASNtuples"
+	triggerDataSetPath = "/nfs/dust2/cms/group/DAS2016/SUSYEdge/CMSDASNtuples"
+	
--- locationsDummy.py
+++
@@ -1,7 +0,0 @@
-class locations:
-	
-	masterListPath = "/home/home4/institut_1b/schomakers/CMSDAS_test/frameWorkBase"
-	dataSetPath = "/disk1/user/schomakers/trees/minimalNtuples"
-	triggerDataSetPath = "/disk1/user/schomakers/trees/minimalNtuples_HT"
-	triggerDataSetPathMC = "/disk1/user/schomakers/trees/minimalNtuples_HT_MC"
-	
