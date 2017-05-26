54b50dcf51a777994686bebf886cfeae46a2aa6b
Christian <cschomak@cern.ch>
2017-05-26 15:57:13 +0200

    Locations file updated

---
+++ locations.py
@@ -0,0 +1,11 @@
+class locations:
+	
+
+	masterListPath = "/home/home4/institut_1b/schomakers/FrameWork/SubmitScripts/Input"
+	dataSetPath = "/disk1/user/schomakers/trees/sw8026v1001_3/"
+	dataSetPathSignal = "/disk1/user/schomakers/trees/sw8026v1001_Signal/"
+	dataSetPathNLL = "/disk1/user/schomakers/trees/sw8026v1001_NLL_2/"
+	dataSetPathSignalNLL = "/disk1/user/schomakers/trees/sw8026v1001_NLL_2/"
+	dataSetPathMC = "/disk1/user/schomakers/trees/sw8026v1001_3/"
+	triggerDataSetPath = "/disk1/user/schomakers/trees/HTTrees_80X/"
+	
